3c7377fd4b7afe4cf11049a329a597d7a49dc9b3
upgradingdave <upgradingdave@gmail.com>
2019-12-17 11:14:59 -0500

    day 7, part 1 with pipes

--- src/advent2019/day7.clj
+++ src/advent2019/day7.clj
@@ -1,121 +1,104 @@
 (ns advent2019.day7
   (:require [clojure.math.combinatorics :as combo]
             [clojure.core.async :as async :refer [go <! >! chan
-                                                  <!! >!! go-loop]]))
-
-;; (def inputs (atom '()))
-
-;; (defn read-next! []
-;;   (let [val (first @inputs)]
-;;     (reset! inputs (rest @inputs))
-;;     (println "NEXT VAL: " val)
-;;     val))
-
-;; (defn write-next! [val]
-;;   (swap! inputs conj val))
+                                                  <!! >!! go-loop
+                                                  pipe alts!]]))
 
 (defn make-opcode [opcode-input]
   (format "%05d" opcode-input))
 
-(defn intcode [program inchan outchan]
-  (go-loop [idx 0
-            result program]
+(defn intcode [program]
+  (let [inchan (chan 1)
+        outchan (chan 1)
+        resultchan (chan)]
+    (go-loop [idx 0
+              result program]
 
-    ;; Read the operation and then we can determine next steps
-    (let [cmd (make-opcode (nth result idx))
-          opcode (Integer/parseInt (str (nth cmd 3) (nth cmd 4)))
-          param1 (= (nth cmd 2) \0)
-          param2 (= (nth cmd 1) \0)
-          param3 (= (nth cmd 0) \0)]
+      ;; Read the operation and then we can determine next steps
+      (let [cmd (make-opcode (nth result idx))
+            opcode (Integer/parseInt (str (nth cmd 3) (nth cmd 4)))
+            param1 (= (nth cmd 2) \0)
+            param2 (= (nth cmd 1) \0)
+            param3 (= (nth cmd 0) \0)]
 
-      ;; (println "idx:" idx)
-      ;; (println "cmd:" cmd)
-      ;; (println "opcode:" opcode)
-      ;; (println "parm1:" param1 ", param2:" param2 ", param3:" param3)
-      ;; (println "result:")
-      ;; (println result)
+        (cond
 
-      (cond
+          ;; add
+          (= opcode 1)
+          (let [[_ x y z] (take 4 (drop idx result))
+                a (if param1 (get result x) x)
+                b (if param2 (get result y) y)
+                result (assoc result z (+ a b))
+                ]
+            (recur (+ 4 idx) result))
 
-        ;; add
-        (= opcode 1)
-        (let [[_ x y z] (take 4 (drop idx result))
-              a (if param1 (get result x) x)
-              b (if param2 (get result y) y)
-              result (assoc result z (+ a b))
-              ]
-          ;; (println "x:" x "y:" y "z:" z)
-          ;; (println "adding a:" a "plus b:" b "storing to z:" z)
-          (recur (+ 4 idx) result))
+          ;; multiply
+          (= opcode 2)
+          (let [[_ x y z] (take 4 (drop idx result))
+                a (if param1 (get result x) x)
+                b (if param2 (get result y) y)
+                result (assoc result z (* a b))
+                ]
+            (recur (+ 4 idx) result))
 
-        ;; multiply
-        (= opcode 2)
-        (let [[_ x y z] (take 4 (drop idx result))
-              a (if param1 (get result x) x)
-              b (if param2 (get result y) y)
-              result (assoc result z (* a b))
-              ]
-          (recur (+ 4 idx) result))
+          ;; read from input and store
+          (= opcode 3)
+          (let [[_ x] (take 2 (drop idx result))]
+            ;;(println "Waiting for input ...")
+            (let [y (<! inchan)
+                  result (assoc result x y)]
+              (recur (+ 2 idx) result)))
 
-        ;; read from input and store
-        (= opcode 3)
-        (let [[_ x] (take 2 (drop idx result))]
-          ;;(println "Waiting for input ...")
-          (let [y (<! inchan)
-                result (assoc result x y)]
-            (recur (+ 2 idx) result)))
+          ;; output
+          (= opcode 4)
+          (let [[_ x] (take 2 (drop idx result))
+                x (if param1 (get result x) x)]
+            (>! outchan x)
+            ;;(println "DIAGNOSTIC CODE: " x)
+            (recur (+ 2 idx) result))
 
-        ;; output
-        (= opcode 4)
-        (let [[_ x] (take 2 (drop idx result))
-              x (if param1 (get result x) x)]
-          (>! outchan x)
-          ;;(println "DIAGNOSTIC CODE: " x)
-          (recur (+ 2 idx) result))
+          ;; jump-if-true
+          (= opcode 5)
+          (let [[_ x y] (take 3 (drop idx result))
+                x (if param1 (get result x) x)
+                y (if param2 (get result y) y)]
+            (if (not (= 0 x))
+              (recur y (assoc result idx y))
+              (recur (+ 3 idx) result)))
 
-        ;; jump-if-true
-        (= opcode 5)
-        (let [[_ x y] (take 3 (drop idx result))
-              x (if param1 (get result x) x)
-              y (if param2 (get result y) y)]
-          (if (not (= 0 x))
-            (recur y (assoc result idx y))
-            (recur (+ 3 idx) result)))
+          ;; jump-if-false
+          (= opcode 6)
+          (let [[_ x y] (take 3 (drop idx result))
+                x (if param1 (get result x) x)
+                y (if param2 (get result y) y)]
+            (if (= 0 x)
+              (recur y (assoc result idx y))
+              (recur (+ 3 idx) result)))
 
-        ;; jump-if-false
-        (= opcode 6)
-        (let [[_ x y] (take 3 (drop idx result))
-              x (if param1 (get result x) x)
-              y (if param2 (get result y) y)]
-          ;; (println "x:" x "y:" y)
-          ;; (println "jump to" y "if x is 0")
-          (if (= 0 x)
-            (recur y (assoc result idx y))
-            (recur (+ 3 idx) result)))
+          ;; less than
+          (= opcode 7)
+          (let [[_ x y z] (take 4 (drop idx result))
+                x (if param1 (get result x) x)
+                y (if param2 (get result y) y)
+                v (if (< x y) 1 0)
+                result (assoc result z v)]
+            (recur (+ 4 idx) result))
 
-        ;; less than
-        (= opcode 7)
-        (let [[_ x y z] (take 4 (drop idx result))
-              x (if param1 (get result x) x)
-              y (if param2 (get result y) y)
-              v (if (< x y) 1 0)
-              result (assoc result z v)]
-          (recur (+ 4 idx) result))
+          ;; equals
+          (= opcode 8)
+          (let [[_ x y z] (take 4 (drop idx result))
+                x (if param1 (get result x) x)
+                y (if param2 (get result y) y)
+                v (if (= x y) 1 0)
+                result (assoc result z v)]
+            (recur (+ 4 idx) result))
 
-        ;; equals
-        (= opcode 8)
-        (let [[_ x y z] (take 4 (drop idx result))
-              x (if param1 (get result x) x)
-              y (if param2 (get result y) y)
-              v (if (= x y) 1 0)
-              result (assoc result z v)]
-          (recur (+ 4 idx) result))
-
-        ;; halt
-        :else
-        nil
-        ;;(println "HALT")
-        ))))
+          ;; halt
+          :else
+          (>! resultchan (alts! [inchan outchan]))
+          )))
+    [inchan outchan resultchan]
+    ))
 
 (defn parse-program [path-to-file]
   (into [] (map #(Integer/parseInt (.trim %1)) (.split (slurp path-to-file) ","))))
@@ -133,36 +116,40 @@
 (comment
 
   ;; provide phase first! and then provide input
-  (def inchan (chan))
-  (def outchan (chan))
-  (go
-    (intcode input1 inchan outchan)
-    (>! inchan 4)
-    (>! inchan 0))
-  (<!! outchan)
+  (def resultchan (chan))
+  (def chans (intcode input1 resultchan))
+  (def inchan (first chans))
+  (def outchan (second chans))
+  (>!! inchan 4)
+  (>!! inchan 0)
 
-  (<!! (thruster-signal input1 [4 3 2 1 0]))
-  
+  (thruster-signal input1 [4 3 2 1 0])
   )
 
-(defn thruster-signal [program phases]
-  (let [inchan (chan)
-        outchan (chan)
-        resultchan (chan)]
-    (go-loop [phase (first phases)
-              phases (rest phases)]
-      (if phase
-        (do
-          (intcode program inchan outchan)
-          (>! inchan phase)
-          (>! inchan (<! outchan))
-          (recur (first phases) (rest phases)))
-        (>! resultchan (<! outchan))))
-    (>!! outchan 0)
+(defn thruster-signal1 [program phases]
+  (let [[amp-a-in amp-a-out _] (intcode program)
+        [amp-b-in amp-b-out _] (intcode program)
+        [amp-c-in amp-c-out _] (intcode program)
+        [amp-d-in amp-d-out _] (intcode program)
+        [amp-e-in amp-e-out resultchan] (intcode program)]
+
+    (pipe amp-a-out amp-b-in)
+    (pipe amp-b-out amp-c-in)
+    (pipe amp-c-out amp-d-in)
+    (pipe amp-d-out amp-e-in)
+
+    (do
+      (>!! amp-a-in (nth phases 0))
+      (>!! amp-b-in (nth phases 1))
+      (>!! amp-c-in (nth phases 2))
+      (>!! amp-d-in (nth phases 3))
+      (>!! amp-e-in (nth phases 4))
+      (>!! amp-a-in 0))
+    
     (<!! resultchan)))
 
 (defn part1 [program]
-  (let [results (map #(thruster-signal program %)
-                 (combo/permutations [0 1 2 3 4]))]
-    (apply max results)))
+  (let [results (map (fn [phases] (thruster-signal1 program phases))
+                     (combo/permutations [0 1 2 3 4]))]
+    (apply max (map first results))))
 
